f0bab32845e547b59c4ecfa0ac2fc2f8742d15a4
Jaymin Darji <jaymindarji2003@gmail.com>
2024-08-01 16:57:34 +0530

    done with the dom

--- basics/DOM  Manipulations/index.js
+++ basics/DOM  Manipulations/index.js
@@ -33,10 +33,37 @@ age.innerText = "age 21";
 function changeBg(newColor) {
     const ele = document.getElementsByTagName("span")[0];
     ele.innerText = ele.innerHTML.toUpperCase();
+    // document.body.style.backgroundColor = newColor;
     ele.style.color = "white";
     ele.style.padding = "10px";
     ele.style.fontWeight = "bold";
     ele.style.backgroundColor = newColor;
 }
 
+const l1 = document.getElementsByTagName("li")[0];
+l1.style.cursor = "pointer";
+l1.addEventListener("click", () => {
+    alert(l1.innerText)
+})
+const l2 = document.getElementsByTagName("li")[1];
+l2.style.cursor = "pointer";
+l2.addEventListener("click", () => {
+    alert(l2.innerText)
+})
+const l3 = document.getElementsByTagName("li")[2];
+l3.style.cursor = "pointer";
+l3.addEventListener("click", () => {
+    alert(l3.innerText)
+})
 
+
+const mydiv = document.querySelector("#mydiv");
+mydiv.style.border = "1px solid black ";
+const button = document.createElement("button");
+button.innerText = "click me";
+// mydiv.appendChild(button)
+button.addEventListener("click", () => {
+    alert("btn clicked")
+})
+
+mydiv.insertAdjacentElement("beforeend", button)
