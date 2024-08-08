19dbe510e6314ad49a34f4d7b12739155b73bc4b
Jaymin Darji <jaymindarji2003@gmail.com>
2024-08-08 11:58:46 +0530

    deep dive into youtube video downloader

---
+++ basics/youtube_video_downloader.js
@@ -0,0 +1,15 @@
+// main website : https://downloaderto.com/engf/
+
+// fetch video details api : https://ab.cococococ.com/ajax/download.php?copyright=0&format=720&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dn6_AU1ZROss&api=dfcb6d76f2f6a9894gjkege8a4ab232222
+
+// download video api : https://p.oceansaver.in/ajax/progress.php?id=ovvBqMP0mgVjRP5KVN1Y4bl
+
+
+
+// testing
+
+// https://ab.cococococ.com/ajax/download.php?copyright=0&format=720&url=https://www.youtube.com/watch?v=AWUDyRmFRQw&api=dfcb6d76f2f6a9894gjkege8a4ab232222
+
+
+// https://p.oceansaver.in/ajax/progress.php?id=2RmtdENPTpGmO69rfJ2HhNw
+
